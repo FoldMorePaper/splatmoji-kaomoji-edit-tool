efddae39b97ada88c8851608226b2d24407f15d4
I.M <=>
2021-08-20 15:08:18 -0300

    almost ready.

--- kaomojitool.py
+++ kaomojitool.py
@@ -13,14 +13,17 @@ Usage:
   For more info visit: https://github.com/iacchus/splatmoji-kaomoji-edit-tool
 """.format(argv[0])
 
+# PROMPTS
 CONSOLE = "(enter kaomoji) "
 COMMAND = "(enter command) "
+RANDOM = "(enter a comma-separated list of keywords; /next to next, /exit to exit)"
 
 COMMANDS =  {
     'add': "Add one or more comma-separated keyword(s) to the kaomoji",
     'back': "Go back to the kaomoji selection console",
     'destroy': "Delete the kaomoji from the database",
     'exit': "Ask to save the changes if there are changes, then exit",
+    'random': "Select a random kaomoji so you can add more keywords to it",
     'rm': "Remove one or more comma-separated keyword(s) to the kaomoji",
     'write': "Backup the database and save the changes to the original",
 }
@@ -40,22 +43,35 @@ if len(argv) < 2:
 filename = argv[1]
 db = KaomojiDB(filename=filename)
 
-print(welcome)
+# kaomoji selection
+while True:
 
-code = input(CONSOLE)
-selected_kaomoji = Kaomoji(code)
-if db.kaomoji_exists(selected_kaomoji):
-    status = "The selected kaomoji exists on the database and has currently",\
-             " {keywords}: {keywords}"
-    kaomoji = db.get_kaomoji_by_code(code)
-else:
-    status = "New kaomoji! Not on the database currently."
-    kaomoji = db.add_kaomoji(selected_kaomoji)
+    print(welcome)
 
-keywords = None
-inside_kaomoji = """
-You chose the kaomoji {code}
+    # prompt 1
+    code = input(CONSOLE)
 
-"""
+    selected_kaomoji = Kaomoji(code)
 
-command = None
+    if db.kaomoji_exists(selected_kaomoji):
+        kaomoji = db.get_kaomoji_by_code(code)
+        num = len(kaomoji.keywords)
+        status = "The selected kaomoji exists on the database and has" \
+                 " currently {num} keywords: {keywords}"\
+                    .format(num=num, keywords=", ".join(kaomoji.keywords))
+    else:
+        status = "New kaomoji! Not on the database currently."
+        kaomoji = db.add_kaomoji(selected_kaomoji)
+
+    #print(status)
+
+    # inside kaomoji
+    while True:
+        inside_kaomoji = """
+        You chose the kaomoji {code}
+        {status}
+        """.format(status=status)
+        print(inside_kaomoji)
+
+        # prompt 2
+        command = input(COMMAND)
