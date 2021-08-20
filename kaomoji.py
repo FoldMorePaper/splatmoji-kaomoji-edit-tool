50a6295337e7ed38bc5e37acde6f633c48565181
I.M <=>
2021-08-20 13:08:41 -0300

    going ok..!

--- kaomoji.py
+++ kaomoji.py
@@ -1,7 +1,8 @@
 from hashlib import sha256
 
+
 class Kaomoji:
-    """Represents an Kaomoji entity."""
+    """Represents a Kaomoji entity."""
 
     code = str()  # unicode of the kaomoji
     keywords = list()  # list of strings
@@ -11,18 +12,19 @@ class Kaomoji:
         self.code = code
 
         if keywords:
-            self.keywords = keywords.split(',')
+            self.keywords = [keyword.strip()
+                             for keyword in keywords.split(',')]
 
         self.hash = self._makehash(code)
 
     def add_keywork(self, keyword):
-        self.keywords.append(keyword)
+        self.keywords.append(keyword.strip())
 
     def _makehash(self, code):
         """Gives a UUID for a given kaomoji, for comparison.
 
         It is the base10 of the sha256 digest of the kaomoji code:
-            HASH = INT10(SHA256(KAOMOJI_CODE))
+            HASH = INT10(SHA256(BYTES(UNICODE_KAOMOJI_CODE_UTF8)))
 
         With this we can know if some emoji is already on the DATABASE, so to
             append keywords to it.
@@ -55,9 +57,10 @@ class Kaomoji:
 class KaomojiDB:
     """Offers facilities to edit and check the DB file."""
 
-    #db_file = None
+    # db_file = None
     filename = str()
-    kaomojis = list()
+    #kaomojis = list()
+    kaomojis = dict()
     entry_num = int()
 
 
@@ -72,7 +75,7 @@ class KaomojiDB:
     def load_file(self, filename):
         """Loads a db file."""
 
-        self.db_file = open(filename)
+        db_file = open(filename, "r")
         #with open(filename) as dbfile:
         lines = db_file.readlines()
 
@@ -84,27 +87,57 @@ class KaomojiDB:
 
             # TODO: FIXME: WE NEED TO CHECK IF KAOMOJI EXISTS TO UPDATE INSTEAD
             #  OF ADDING, HERE:
-            self.kaomojis.append(kaomoji)
+            #self.kaomojis.append(kaomoji)
+            self.kaomojis.update({code: kaomoji})
+
+        self.entry_num = len(self.kaomojis)
 
 
     def write(self):
         """Writes a db file."""
 
-        for kaomoji in self.kaomojis:
+        for kaomoji in self.kaomojis.values():
             code = kaomoji.code
             keywords_string = ",".join(kaomoji.keywords)
             db_line = "{0}\t{1}\n".format(code, keywords_string)
+            # TODO: do me
 
 
-    def kaomoji_exists(self, kaomoji: Kaomoji):
+    def kaomoji_exists(self, other: Kaomoji):
         """Checks if a kaomoji exists already in the database."""
-        pass
+
+        if other.code in self.kaomojis.keys():
+        # for kaomoji in self.kaomojis:
+        #     if other == kaomoji:
+                return True
+
+        return False
 
 
     def add_kaomoji(self, kaomoji: Kaomoji):
         """Adds a Kaomoji to the database."""
-        pass
+        self.kaomojis.update({kaomoji.code: kaomoji})
 
+
+    def get_kaomoji_by_code(self, code: str):
+        """Gets a Kaomoji with it's current keywords from the database."""
+        return self.kaomojis[code]
+        # for kaomoji in self.kaomojis:
+        #     if code == kaomoji.code:
+        #         return kaomoji
+
+    def get_kaomoji_by_kaomoji(self, other: Kaomoji):
+        """Gets a Kaomoji with it's current keywords from the database."""
+        # for kaomoji in self.kaomojis.values():
+        #     if other == kaomoji:
+        #         return kaomoji
+        return self.kaomojis[other.code]
+
+    def get_kaomoji_by_hash(self, thehash: int):
+        """Gets a Kaomoji with it's current keywords from the database."""
+        for kaomoji in self.kaomojis.values():
+            if thehash == kaomoji.hash:
+                return kaomoji
 
     def remove_kaomoji(self, kaomoji: Kaomoji):
         """Removes a Kaomoji from the database."""
@@ -115,17 +148,3 @@ class KaomojiDB:
         """Updates keywords to database."""
         pass
 
-
-
-
-# db = KaomojiDB(file=DB_FILE)
-#
-# code = input("code pls?: ")
-#
-# new_kaomoji = Kaomoji(code)
-# print(new_kaomoji)
-# print(new_kaomoji.hash)
-#
-# while True:
-#     keyword = input("Type new keyword (CTRL-C to END): ")
-#     new_kaomoji.add_keywork(keyword)
--- tests.py
+++ tests.py
@@ -1,18 +1,38 @@
 from kaomoji import KaomojiDB
 from kaomoji import Kaomoji
 
-DB_FILE = "kaomoji.txt"
+#DB_FILE = "kaomoji.txt"
+DB_FILE = "emoticons.tsv"
 
 #db = KaomojiDB(filename=DB_FILE)
+db = KaomojiDB()
+db.load_file(filename=DB_FILE)
+
+print(db.entry_num, "kaomojis loaded")
 
 code = input("code pls?: ")
 
 new_kaomoji = Kaomoji(code)
-print(new_kaomoji)
-print(new_kaomoji.hash)
+
+# print(new_kaomoji)
+# print(new_kaomoji.hash)
+
+if db.kaomoji_exists(new_kaomoji):
+    print("Already on the database")
+    # return the kaomoji with the keywords if it has some
+    new_kaomoji = db.get_kaomoji_by_kaomoji(new_kaomoji)
+    print("The kaomoji already exists with the keywords: ",
+          ",".join(new_kaomoji.keywords))
+else:
+    print("Not on the database yet.")
 
 while True:
+
     keyword = input("Type new keyword (enter /end to end): ")
+
     if keyword == "/end":
         break
+
     new_kaomoji.add_keywork(keyword)
+
+db.update_kaomoji(kaomoji=new_kaomoji)
